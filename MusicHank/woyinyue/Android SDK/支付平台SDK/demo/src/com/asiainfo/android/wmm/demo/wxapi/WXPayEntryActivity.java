e08707f50de25ea9e343590119194f2bcbc202ed
shanggl <guilong.shang@beantechs.com>
2020-07-29 18:29:10 +0800

    删除沃音乐相关文件

--- MusicHank/woyinyue/Android SDK/支付平台SDK/demo/src/com/asiainfo/android/wmm/demo/wxapi/WXPayEntryActivity.java
+++
@@ -1,8 +0,0 @@
-package com.asiainfo.android.wmm.demo.wxapi;
-
-
-import com.asiainfo.android.wo.music.mall.activity.WxPayResultActivity;
-
-public class WXPayEntryActivity extends WxPayResultActivity {
-
-}
